3e75de75de5a9ac046a4da5380155b2bf2c42c50
T-Jay Oliver <tjoliver1@yahoo.com>
2023-04-19 10:06:47 +0100

    Added PORT Number number and data.sql to gitignore

--- server/database/database.js
+++ server/database/database.js
@@ -7,7 +7,8 @@ export const database = mysql.createPool({
     host : process.env.HOST,
     user : process.env.USER,
     password : process.env.PASSWORD,
-    database : process.env.DATABASE
+    database : process.env.DATABASE,
+    port : process.env.PORT
 });
 
 
